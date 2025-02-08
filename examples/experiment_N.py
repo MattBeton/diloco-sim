a59b11f4c4306da4ecdb1def7d5c428a5842a379
Matt Beton <matthew.beton@gmail.com>
2025-02-08 09:04:18 +0000

    Worked on experiments.

--- examples/experiment_N.py
+++ examples/experiment_N.py
@@ -22,20 +22,17 @@ def run_command(command):
         sys.exit(1)
 
 def main():
-    # TODO: cosine annealing
-    global_command = 'python transformer.py --train --port 12355 --wandb_project owt_diloco_n --batch_size 16 --max_local_step 100 --corr_interval 100000'
-
     devices = '0 1 2 3'
-    num_nodes = 4
-    diloco_interval = 100
 
-    for n in [2, 4, 8, 16]:
-        command1 = f'--num_nodes 1 --devices {devices} --p_sparta 0 --diloco_interval {diloco_interval} --wandb_name '
-        run_command(f'{global_command} {command1}')
+    global_command = f'python transformer.py --cosine_anneal --train --port 12355 --wandb_project owt_diloco_H_full --model_size base --batch_size 16 --max_local_step 30000 --corr_interval 100000 --devices {devices}'
 
-        command2 = f'--num_nodes {num_nodes} --devices {devices} --p_sparta 0.002 --diloco_interval {diloco_interval} --wandb_name dlc{diloco_interval}'
-        run_command(f'{global_command} {command2}')
+    H = 100
+    num_nodes = 8
+    command1 = f' --num_nodes {num_nodes} --p_sparta 0 --diloco_interval {H} --wandb_name dlc{H}'
+    run_command(f'{global_command} {command1}')
 
+    command2 = f' --num_nodes {num_nodes} --p_sparta 0.005 --diloco_interval {H} --wandb_name dlc{H}_p0.005'
+    run_command(f'{global_command} {command2}')
 
 
 if __name__ == "__main__":
