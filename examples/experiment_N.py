b69bf3e6ff2443fc27905d8802ad537b4be7a754
Matt Beton <matthew.beton@gmail.com>
2025-02-08 09:06:49 +0000

    more changes to experiment N

--- examples/experiment_N.py
+++ examples/experiment_N.py
@@ -24,14 +24,14 @@ def run_command(command):
 def main():
     devices = '0 1 2 3'
 
-    global_command = f'python transformer.py --cosine_anneal --train --port 12355 --wandb_project owt_diloco_H_full --model_size base --batch_size 16 --max_local_step 30000 --corr_interval 100000 --devices {devices}'
+    global_command = f'python transformer.py --cosine_anneal --train --port 12355 --wandb_project owt_diloco_N --model_size base --batch_size 16 --max_local_step 30000 --corr_interval 100000 --devices {devices}'
 
     H = 100
     num_nodes = 8
-    command1 = f' --num_nodes {num_nodes} --p_sparta 0 --diloco_interval {H} --wandb_name dlc{H}'
+    command1 = f' --num_nodes {num_nodes} --p_sparta 0 --diloco_interval {H} --wandb_name n{num_nodes}_dlc{H}'
     run_command(f'{global_command} {command1}')
 
-    command2 = f' --num_nodes {num_nodes} --p_sparta 0.005 --diloco_interval {H} --wandb_name dlc{H}_p0.005'
+    command2 = f' --num_nodes {num_nodes} --p_sparta 0.005 --diloco_interval {H} --wandb_name n{num_nodes}_dlc{H}_p0.005'
     run_command(f'{global_command} {command2}')
 
 
